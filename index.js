f01d3fe7bf22a1a214883b7e63e2073b05c3206a
skylerlee <skyler.ac.lee@gmail.com>
2018-02-02 21:41:33 +0800

    Add plugin file

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2017, Skyler.
+ * Use of this source code is governed by the MIT license that can be
+ * found in the LICENSE file.
+ */
+
+export default function typescript (options) {
+  return {}
+}
