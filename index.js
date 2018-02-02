efa546299098ef5180562a6f13cfce8d28c755b3
skylerlee <skyler.ac.lee@gmail.com>
2018-02-02 22:23:50 +0800

    Update plugin

--- index.js
+++ index.js
@@ -4,6 +4,35 @@
  * found in the LICENSE file.
  */
 
+import ts from 'typescript'
+import utils from 'rollup-pluginutils'
+
 export default function typescript (options) {
-  return {}
+  options = assign({}, default, options)
+  let confFile = findup('tsconfig.json')
+  let tsConfig = fs.readJSONSync(confFile)
+  let compilerOptions = getCompilerOptions(tsConfig)
+
+  return {
+    resolveId (importee, importer) {
+      if (!importer) { return null }
+      let result = ts.nodeModuleNameResolver(
+        importee,
+        importer,
+        compilerOptions,
+        resolveHost)
+      return _.get(result, 'resolvedModule.resolvedFileName', null)
+    },
+    transform (code, id) {
+      let compiled = ts.transpileModule(code, {
+        fileName: id,
+        reportDiagnostics: true,
+        compilerOptions
+      })
+      return {
+        code: compiled.outputText,
+        map: JSON.parse(compiled.sourceMapText)
+      }
+    }
+  }
 }
