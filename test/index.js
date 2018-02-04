2bf28a2ded4c134f5de3ffbc6f4ae9e0d4f9783b
skylerlee <skyler.ac.lee@gmail.com>
2018-02-04 16:51:37 +0800

    Update test file

---
+++ test/index.js
@@ -0,0 +1,17 @@
+const rollup = require('rollup')
+const tsPlugin = require('..')
+
+function bundle (input) {
+  let options = {
+    input,
+    plugins: [
+      tsPlugin()
+    ]
+  }
+  return new Promise((resolve, reject) => {
+    rollup.rollup(options).then(resolve, reject)
+  })
+}
+
+describe('rollup-plugin-typescript', function () {
+})
