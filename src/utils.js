5644b2318b4be51e2a0b06f2adbbeb25b28ac4e6
skylerlee <skyler.ac.lee@gmail.com>
2018-02-07 15:58:03 +0800

    Fix bugs

--- src/utils.js
+++ src/utils.js
@@ -4,6 +4,7 @@
  * found in the LICENSE file.
  */
 
+const _toString = Object.prototype.toString
 const _toLowerCase = String.prototype.toLowerCase
 
 export function lowerCase (str) {
@@ -15,25 +16,30 @@ export function isObject (value) {
   return value !== null && (type === 'object' || type === 'function')
 }
 
+export function isArray (value) {
+  return _toString.call(value) === '[object Array]'
+}
+
 export function mergeObject (dst, ...srcs) {
   if (!isObject(dst)) {
     throw new TypeError('First argument must be an object')
   }
   for (let i = 0, len = srcs.length; i < len; i++) {
     let src = srcs[i]
-    try {
-      JSON.stringify(src)
-    } catch (e) {
-      throw e
-    }
+    if (src == null) { continue }
     let keys = Object.keys(src)
     for (let j = 0, len = keys.length; j < len; j++) {
       let key = keys[j]
       let value = src[key]
       if (!isObject(value)) {
         dst[key] = value
-      } else { // is object
-        mergeObject((dst[key] = {}), value)
+      } else { // value is object
+        if (isArray(value) && !isArray(dst[key])) {
+          dst[key] = []
+        } else if (!isObject(dst[key])) {
+          dst[key] = {}
+        }
+        mergeObject(dst[key], value)
       }
     }
   }
