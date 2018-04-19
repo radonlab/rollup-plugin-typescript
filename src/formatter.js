56020cbf50b51fcaa21b4e08ddc6839763ef2e8a
skylerlee <skyler.ac.lee@gmail.com>
2018-04-19 22:59:41 +0800

    Update formatter

--- src/formatter.js
+++ src/formatter.js
@@ -4,5 +4,14 @@
  * found in the LICENSE file.
  */
 
+import path from 'path'
+import ts from 'typescript'
+
 export function formatDiagnostic (diagnostic) {
+  let source = diagnostic.file
+  let pos = source.getLineAndCharacterOfPosition(diagnostic.start)
+  let name = path.relative(process.cwd(), source.fileName)
+  let message = ts.flattenDiagnosticMessageText(diagnostic.messageText)
+  return `TS${diagnostic.code} ${name}(${pos.line + 1}:${pos.character + 1})
+  ${message}`
 }
