021677bdd2d8289c0aa083b03e12598d580df7d4
skylerlee <skyler.ac.lee@gmail.com>
2018-06-14 01:42:27 +0800

    Update build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,14 @@
 'use strict'
 const rollup = require('rollup')
+const config = require('../rollup.config')
 
-module.exports = function build () {
+function build () {
+  rollup.rollup(config).then(bundle => {
+    bundle.write(config.output)
+  })
 }
+
+module.exports = build
 
 if (require.main === module) {
   build()
