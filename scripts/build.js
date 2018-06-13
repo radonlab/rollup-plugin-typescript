f509708e1fd09bff599b80cfb06261871baad7b4
skylerlee <skyler.ac.lee@gmail.com>
2018-06-14 02:05:29 +0800

    Update build

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,27 @@
 'use strict'
 const rollup = require('rollup')
 const config = require('../rollup.config')
+const pkginfo = require('../package')
+
+function keys (obj) {
+  if (!obj) {
+    return []
+  }
+  return Object.keys(obj)
+}
+
+function getDependencies (pkg) {
+  let deps = keys(pkg.dependencies)
+  let peerDeps = keys(pkg.peerDependencies)
+  return deps.concat(peerDeps)
+}
 
 function build () {
+  // process config
+  let external = config.external || []
+  let pkgDeps = getDependencies(pkginfo)
+  config.external = external.concat(pkgDeps)
+  // bundle modules
   rollup.rollup(config).then(bundle => {
     bundle.write(config.output)
   })
