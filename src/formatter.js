1d0a690fcb34f87a3814317dbf7ccff9c081c4ce
skylerlee <skyler.ac.lee@gmail.com>
2018-06-23 22:23:35 +0800

    Fix diagnostic format function

--- src/formatter.js
+++ src/formatter.js
@@ -13,11 +13,14 @@ export function formatError (err) {
 }
 
 export function formatDiagnostic (diagnostic) {
-  let source = diagnostic.file
-  let pos = source.getLineAndCharacterOfPosition(diagnostic.start)
-  let name = path.relative(process.cwd(), source.fileName)
+  let code = diagnostic.code
   let message = ts.flattenDiagnosticMessageText(diagnostic.messageText)
-  return cl.red(`  TS${diagnostic.code}  `) +
-    `${name}(${pos.line + 1}:${pos.character + 1})\n` +
-    cl.cyan(`  ${message}`)
+  let file = diagnostic.file
+  let fileLine = ''
+  if (file) {
+    let name = path.relative(process.cwd(), file.fileName)
+    let pos = file.getLineAndCharacterOfPosition(diagnostic.start)
+    fileLine = `${name}(${pos.line + 1}:${pos.character + 1})`
+  }
+  return `  ${cl.red('TS' + code)}  ${fileLine}\n  ${cl.cyan(message)}`
 }
